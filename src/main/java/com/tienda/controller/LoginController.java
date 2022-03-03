83349da4763cee0fe6b61f21ffeb6be9c1d90f20
Hewlett Packard <Hewlett Packard@DESKTOP-3P4P977>
2022-03-02 23:55:03 -0600

    Versión del proyecto Tienda de la semana 5, trabajo en clase

--- src/main/java/com/tienda/controller/LoginController.java
+++ src/main/java/com/tienda/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
     
-    @GetMapping({"/","/perro"})
+    @GetMapping({"/","/login"})
     public String index(){
     return "login";
     
--- src/main/java/com/tienda/controller/PersonasController.java
+++ src/main/java/com/tienda/controller/PersonasController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
   
 
 
@@ -15,11 +18,32 @@ public class PersonasController {
     @Autowired
     private IPersonaService personaService;
      
-    @GetMapping("/personas")
-    public String personas(Model model){
+ 
+     @GetMapping("/persona")
+    public String index(Model model){
     List<Persona> listaPersonas=personaService.getAllPerson();
     model.addAttribute("titulo","Personas");
     model.addAttribute("personas",listaPersonas);
     return "personas";
     }
+     @GetMapping("/personasN")
+    public String crearPersona(Model model){
+    List<Persona> listaPersonas=personaService.getAllPerson();
+    model.addAttribute("persona", new Persona());
+    //personaService.savePerson(persona);
+    return "crear";
+    }
+    
+      @GetMapping("/save")
+    public String guardarPersona(@ModelAttribute Persona persona){
+    personaService.savePerson(persona);
+    return "redirect:/persona";
+    }
+    
+     @GetMapping("/delete/{id}")
+    public String eliminarPersona(@PathVariable("id") Long idPersona){
+    personaService.delete(idPersona);
+    return "redirect:/persona";
+    }
+     
 }
